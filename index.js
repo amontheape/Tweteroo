d646d88a146d922f8b724e921d336d9ce409f29a
amontheape <mcanedotome@gmail.com>
2022-01-23 19:41:58 -0300

    feat: tweets endpoint

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ app.use(express.json());
 app.use(cors());
 
 let user = [];
+let tweets = [];
 
 app.post('/sign-up', (req, res) => {
   user.push(req.body);
-  console.log(user);
   res.send("OK");
+})
+
+app.post('/tweets', (req, res) => {
+  tweets.push( { ...req.body, avatar: user[0].avatar } );
+  res.send("OK");
+})
+
+app.get('/tweets', (req , res) => {
+  res.send(tweets.slice(-10).reverse());
 })
 
 app.listen(5000);
